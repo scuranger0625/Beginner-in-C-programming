ae4be1463872a2913d1ca7d5b9168d1c6f61739f
Leon <166517099+scuranger0625@users.noreply.github.com>
2024-10-22 13:43:37 +0800

    Add files via upload

---
+++ 常用函式庫.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //表準輸入與輸出
+#include <ctype.h> // 字元分類測試
+#include <strings.h> //字串處理與轉換
+#include <math.h> //數學函式
+#include <stdlib.h> // 標準函式庫 提供各類基本函式
+#include <assert.h> // 例外偵錯 有助於除錯
+#include <stdarg.h> // 引數串列的測試
+#include <setjmp.h> // 非區域跳躍
+#include <signal.h> // 訊號偵錯
+#include <time.h> // 提供各類時間函式
+#include <limits.h> // 提供某些極限值的定義
+#include <float.h>  // 提供浮點數的精確位數定義
